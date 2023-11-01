dc1fec98d24c4b7b0e292104204988db3522eda5
“Sergey” <s22-01-15@yandex.ru>
2023-11-01 13:21:40 +1000

    task1

--- homeworks/homework3/src/task1/main/Task1.java
+++ homeworks/homework3/src/task1/main/Task1.java
@@ -1,5 +1,5 @@
 public class Task1 {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static boolean evenOddNumber(int n) {
+        return (n % 2 == 0);
     }
 }
--- homeworks/homework3/src/task1/testsources/TestTask1.java
+++ homeworks/homework3/src/task1/testsources/TestTask1.java
@@ -1,2 +1,17 @@
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class TestTask1 {
+    @ParameterizedTest
+    @ValueSource (ints = {2, 8, 26, 88})
+    void testEvenOddNumberTrue (int n){
+        assertThat(Task1.evenOddNumber(n)).isEqualTo(true);
+    }
+    @ParameterizedTest
+    @ValueSource (ints = {3, 11, 27, 83})
+    void testEvenOddNumberFalse (int n){
+        assertThat(Task1.evenOddNumber(n)).isEqualTo(false);
+    }
 }
